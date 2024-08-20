4f7f1a88d9db7af77f045ef1f702278e9f529e91
Chris2Rich <AzkronTheGamer@gmail.com>
2024-08-20 01:40:06 +0100

    made everything work?

--- api/face_service.js
+++ api/face_service.js
@@ -1,23 +1,54 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://database:password@raterandoms.uyloh.mongodb.net/?retryWrites=true&w=majority&appName=raterandoms";
+const axios = require('axios');
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const day = Math.floor(((new Date().getTime()) - 1724022000000) / 86400000)
+let daily_data = null
 
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-  } finally {
-    await client.close();
-  }
+const data_get = JSON.stringify({
+  "collection": "data",
+  "database": "data",
+  "dataSource": "raterandoms",
+  "pipeline": [{
+    $project: {
+      nthObject: { $arrayElemAt: ["$data", day] }
+    }
+  }]
+})
+
+let data_post = null
+
+const config_get = {
+  method: 'post',
+  url: 'https://eu-west-2.aws.data.mongodb-api.com/app/data-yiwnmcn/endpoint/data/v1/action/aggregate',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'api-key': 'cmuCsQRYeNVmJEs1JrvWFPES1ewgP8Q6ikgQM2KxKvheQhjqm6J2DJplEgzJtxBO',
+  },
+  data: data_get
 }
-run().catch(console.dir);
+
+const config_post = {
+  method: 'post',
+  url: 'https://eu-west-2.aws.data.mongodb-api.com/app/data-yiwnmcn/endpoint/data/v1/action/updateOne',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'api-key': 'cmuCsQRYeNVmJEs1JrvWFPES1ewgP8Q6ikgQM2KxKvheQhjqm6J2DJplEgzJtxBO',
+  },
+  data: data_post
+}
+
+async function get_data(){
+  const response = await axios(config_get)
+  daily_data = response.data.documents[0].nthObject
+  return
+}
+
+async function post_data(){
+  const response = await axios(config_post)
+  await get_data()
+  return
+}
 
 function parse_url(s){
   let res_buffer = []
@@ -37,11 +68,38 @@ function parse_url(s){
   return res_buffer.slice(1)
 }
 
-const sheet = "https://docs.google.com/spreadsheets/d/1anARMqhMX6i8BlS-fdE9vzIalkY-9czM6ixZYQsYPUA/edit?gid=0#gid=0"
-
-module.exports = (req, res) => {
-  const data = parse_url(req.url)
-  if(data[0] == "rate"){
-    res.status(200).json({data : parse_url(req.url)})
+module.exports = async (req, res) => {
+  if(daily_data == null){
+    await get_data()
   }
+
+  const url_data = parse_url(req.url)
+  switch(url_data[0]){
+    case "rate":
+      Object.assign(config_post, {data: JSON.stringify({
+        "collection": "data",
+        "database": "data",
+        "dataSource": "raterandoms",
+        "filter": { "_id": { "$oid": "66c388c15108060f7786c50c" } },
+        "update": {
+          "$set": {
+          [`data.${day}.r`]: ((daily_data["r"] * daily_data["n"]) + Number(url_data[2])) / (daily_data["n"] + 1),
+          [`data.${day}.n`]: (daily_data["n"] + 1)
+          }
+        }
+      })})
+
+      await post_data()
+
+      res.status(200).json(daily_data)
+      console.log(daily_data, url_data[2])
+      break
+    case "face":
+      res.status(200).json(daily_data)
+      break
+    default:
+      res.status(404).json({error: 404})
+      break
+  }
+  return
 }
